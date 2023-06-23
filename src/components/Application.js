b6c134b64e1cda964850fc13f68efd903af6d067
getenesh <getenesh.guta@gmail.com>
2023-06-23 18:46:46 -0400

    useApplicationData hook

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,108 +1,22 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-
+import React from "react";
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
-import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "./helper/selectors";
+import useApplicationData from "hooks/useApplicationData";
+import {getInterview } from "./ /selectors";
 
 export default function Application(props) {
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {}
-  });
+  const {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview,
+    interviewers,
+    dailyAppointments
+  } = useApplicationData();
 
-  const dailyAppointments = getAppointmentsForDay(state, state.day);
-  const setDay = (day) => {
-
-    setState((prev) => ({ ...prev, day }));
-
-  };
-
-  useEffect(() => {
-    Promise.all([
-      axios.get("http://localhost:8001/api/days"),
-      axios.get("http://localhost:8001/api/appointments"),
-      axios.get("http://localhost:8001/api/interviewers")
-    ]).then((all) => {
-      const [daysResponse, dailyAppointments, interviewersResponse] = all;
-      setState((prev) => ({
-        ...prev,
-        days: daysResponse.data,
-        appointments: dailyAppointments.data,
-        interviewers: interviewersResponse.data
-      }));
-    });
-  }, []);
-
-  function bookInterview(id, interview) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-  
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-  
-    // Return the promise
-    return axios.put(`http://localhost:8001/api/appointments/${id}`, { interview })
-      .then((response) => {
-        if (response.status === 204) {
-          // Update the state with the updated appointment data
-          setState({
-            ...state,
-            appointments
-          });
-          return true;
-        } else {
-          throw new Error('Failed to update appointment');
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-        // Handle error if the request fails
-        throw error; // Propagate the error to the caller
-      });
-  }
-
-  function cancelInterview(id) {
-    // Make the put request using Axios
-    return axios.delete(`http://localhost:8001/api/appointments/${id}`)
-      .then((response) => {
-        if (response.status === 204) {
-          // Update the state with the cancelled appointment data
-          setState(prev => ({
-            ...prev,
-            appointments: {
-              ...prev.appointments,
-              [id]: {
-                ...prev.appointments[id],
-                interview: null
-              }
-            }
-          }));
-          
-          return true;
-        } else {
-          throw new Error('Failed to cancel appointment');
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-        // Handle error if the request fails
-      });
-  }
-
-  
-
-  // Move the schedule mapping outside of the JSX block
-  const schedule = dailyAppointments.map((appointment) => {
+  const appointments = dailyAppointments.map((appointment) => {
     const interview = getInterview(state, appointment.interview);
-    const interviewers = getInterviewersForDay(state, state.day);
     return (
       <Appointment
         key={appointment.id}
@@ -115,8 +29,6 @@ export default function Application(props) {
       />
     );
   });
-
-
 
   return (
     <main className="layout">
@@ -137,7 +49,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {schedule}
+        {appointments}
         <Appointment key="last" time="5pm" />
       </section>
     </main>
---
+++ src/hooks/useApplicationData.js
@@ -0,0 +1,110 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import {
+  getAppointmentsForDay,
+  getInterviewersForDay
+} from "../components/helper/selectors";
+
+export default function useApplicationData() {
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {}
+  });
+
+  const setDay = (day) => {
+    setState((prev) => ({ ...prev, day }));
+  };
+
+  useEffect(() => {
+    Promise.all([
+      axios.get("http://localhost:8001/api/days"),
+      axios.get("http://localhost:8001/api/appointments"),
+      axios.get("http://localhost:8001/api/interviewers")
+    ]).then((all) => {
+      const [daysResponse, appointmentsResponse, interviewersResponse] = all;
+      setState((prev) => ({
+        ...prev,
+        days: daysResponse.data,
+        appointments: appointmentsResponse.data,
+        interviewers: interviewersResponse.data
+      }));
+    });
+  }, []);
+
+  function bookInterview(id, interview) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+
+    // Return the promise
+    return axios
+      .put(`http://localhost:8001/api/appointments/${id}`, { interview })
+      .then((response) => {
+        if (response.status === 204) {
+          // Update the state with the updated appointment data
+          setState((prev) => ({
+            ...prev,
+            appointments
+          }));
+          return true;
+        } else {
+          throw new Error("Failed to update appointment");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        // Handle error if the request fails
+        throw error; // Propagate the error to the caller
+      });
+  }
+
+  function cancelInterview(id) {
+    // Make the delete request using Axios
+    return axios
+      .delete(`http://localhost:8001/api/appointments/${id}`)
+      .then((response) => {
+        if (response.status === 204) {
+          // Update the state with the cancelled appointment data
+          setState((prev) => ({
+            ...prev,
+            appointments: {
+              ...prev.appointments,
+              [id]: {
+                ...prev.appointments[id],
+                interview: null
+              }
+            }
+          }));
+
+          return true;
+        } else {
+          throw new Error("Failed to cancel appointment");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        // Handle error if the request fails
+        throw error; // Propagate the error to the caller
+      });
+  }
+
+  const interviewers = getInterviewersForDay(state, state.day);
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+
+  return {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview,
+    interviewers,
+    dailyAppointments
+  };
+}
