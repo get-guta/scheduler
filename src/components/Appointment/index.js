5bed0c4d0f3d5e75d72239b16e7b7d7b8b04191d
getenesh <getenesh.guta@gmail.com>
2023-06-23 16:11:15 -0400

    saving status

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,13 +4,15 @@ import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
+import Status from "./Status";
 import useVisualMode from "../../hooks/useVisualMode";
-import { getAppointmentsForDay } from "../helper/selectors";
 
 export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
+  const SAVING = "SAVING";
+
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
@@ -19,8 +21,17 @@ export default function Appointment(props) {
       student: name,
       interviewer
     };
-    props.bookInterview(props.id,interview);
-    transition(SHOW);
+    transition(SAVING); // Transition to SAVING mode before calling bookInterview
+
+    props.bookInterview(props.id, interview)
+      .then(() => {
+        transition(SHOW); // Transition to SHOW mode when the request is complete
+      })
+      .catch((error) => {
+        console.error(error);
+        // Handle error if the request fails
+      });
+
   }
 
   return (
@@ -37,8 +48,10 @@ export default function Appointment(props) {
         {mode === CREATE && (<Form
           interviewers={props.interviewers}
           onSave={save}
-          onCancel ={() => back(EMPTY)}
+          onCancel={() => back(EMPTY)}
         />)}
+        {mode === SAVING && <Status message="Saving" />}
+
       </article>
 
     </>
